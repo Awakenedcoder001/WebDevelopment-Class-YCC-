061da84395389ec34b50380de564ddbba6e71f37
Confusedcoderfrombhutan <92351601+Awakenedcoder001@users.noreply.github.com>
2022-01-04 15:37:24 +0530

    Delete writelines.py

--- writelines.py
+++
@@ -1,8 +0,0 @@
-def write_lines(): 
- write_lines=open("writelines.txt","w") 
- multiline=["Sachin\n","Dravid\n","Gavaskar\n","Michael Bevan\n","Lara\n","Ponting"] 
- write_lines.writelines(multiline) 
- write_lines.close() 
- 
-if __name__=="__main__": 
- write_lines()
