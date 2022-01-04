8a364056be9046d82388d40bbb67f0019fe6136d
Confusedcoderfrombhutan <92351601+Awakenedcoder001@users.noreply.github.com>
2022-01-04 15:37:43 +0530

    Delete writelinetrial.py

--- writelinetrial.py
+++
@@ -1,9 +0,0 @@
-def writelines():
-    write_lines=open("writelines.txt","a")\n
-    multiline=["Tandin","Tshewang", "Male" "Dota2"]
-    write_lines.writelines(multiline)
-    write_lines.close()
-
-
-if __name__ == "__main__":
-    writelines()
