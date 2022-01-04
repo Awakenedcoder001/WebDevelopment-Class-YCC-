bc5b03306cf26139a19ceb6db3c4b413972f646d
Confusedcoderfrombhutan <92351601+Awakenedcoder001@users.noreply.github.com>
2022-01-04 15:36:24 +0530

    Delete read_operation.py

--- read_operation.py
+++
@@ -1,9 +0,0 @@
-def read_operation(): 
- read_file=open("writing3.txt","r") 
- print("File is openened for reading") 
- file_contents=read_file.read(10) 
- print(file_contents) 
- read_file.close() 
- 
-if __name__== '__main__': 
- read_operation()
